cd0bbc19f3f4f85c070f786e3a2499512cdf7e75
xxristoskk <xristos.kvtsvros@gmail.com>
2020-02-20 20:47:31 -0500

    mod procfile

--- tw33tyPyElections/tw33tyPyElections/wsgi.py
+++ tw33tyPyElections/tw33tyPyElections/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tw33tyPyElections.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tw33tyPyElections.tw33tyPyElections.settings')
 
 application = get_wsgi_application()
