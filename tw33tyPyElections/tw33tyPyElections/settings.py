7e1c1a5ec9ca7405bc9d9d2e5513866e1bc3721d
xxristoskk <xristos.kvtsvros@gmail.com>
2020-02-22 16:16:54 -0500

    changed settings

--- tw33tyPyElections/tw33tyPyElections/settings.py
+++ tw33tyPyElections/tw33tyPyElections/settings.py
@@ -22,9 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# secret = os.environ['secret']
+secret = os.environ['secret']
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret
+SECRET_KEY = ecret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
