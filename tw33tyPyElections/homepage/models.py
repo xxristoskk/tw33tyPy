e78bb1c976827b08f45451fa740fbcf001467236
xxristoskk <xristos.kvtsvros@gmail.com>
2020-02-22 17:09:41 -0500

    staticfiles changed

--- tw33tyPyElections/homepage/models.py
+++ tw33tyPyElections/homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django_plotly_dash import DjangoDash
 import datetime
+
+class StatelessApp(models.Model):
+    app_name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    slug = models.SlugField(max_length=110, unique=True, blank=True)
+
+    def as_dash_app(self):
+        return app_name
