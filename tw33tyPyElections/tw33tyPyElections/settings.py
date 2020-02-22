38af9806dcceea077f9cde6c8d9b8f1ad8b2da96
xxristoskk <xristos.kvtsvros@gmail.com>
2020-02-22 16:05:07 -0500

    changed database

--- tw33tyPyElections/tw33tyPyElections/settings.py
+++ tw33tyPyElections/tw33tyPyElections/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-# from homepage import config
+from homepage import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-secret = os.environ['secret']
+# secret = os.environ['secret']
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret
+SECRET_KEY = config.secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
-        'PORT': '',                    
+        'PORT': '',
     }
 }
 
